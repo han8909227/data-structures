8dedbee8a5c61f10b0542bcf5b13d7e87053ae74
Philip Werner <philip.r.werner@gmail.com>
2017-10-24 16:57:30 -0700

    working the right way

--- src/stack.py
+++ src/stack.py
@@ -31,29 +31,23 @@ class Stack(object):
 
     def __init__(self, iterable=()):
         """Will init a new instance of the Stack class."""
+        from linked_list import LinkedList
         self.top_stack = None
         self._counter = 0
+        self._linked_list = LinkedList()
         if isinstance(iterable, (str, tuple, list)):
             for item in iterable:
                 self.push(item)
 
     def push(self, new_stack):
         """Will push a new element into the Stack and add to the counter."""
-        new_node = Node(new_stack, self.top_stack)
-        new_node.set_next(self.top_stack)
-        self.top_stack = new_node
-        self._counter += 1
+        self._linked_list.insert(new_stack)
 
     def pop(self):
         """Will pop the element that is in the top stack position and subtract it from the counter."""
-        try:
-            output = self.top_stack.data
-            self.top_stack = self.top_stack.get_next()
-            self._counter -= 1
-            return output
-        except AttributeError:
-            raise Exception('Nothing to pop.')
+        return self._linked_list.pop()
+
 
     def __len__(self):
         """Will return the length of the stack using len()."""
-        return self._counter
+        return self._linked_list.size()
