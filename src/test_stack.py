6075f84eecb02e32d567557a6b9d96fad290cdb4
Philip Werner <philip.r.werner@gmail.com>
2017-10-24 23:03:47 -0700

    tried fixing pop empty test

--- src/test_stack.py
+++ src/test_stack.py
@@ -6,7 +6,6 @@ from linked_list import LinkedList
 
 def test_init_new_stack():
     """Will test that the instance is initialized."""
-
     a = Stack()
     assert hasattr(a, 'top_stack')
     assert isinstance(a, Stack)
@@ -15,14 +14,12 @@ def test_init_new_stack():
 
 def test_stack_init_with_iterable():
     """Will test that input is iterable."""
-
     a = Stack((1, 2, 3, 4))
     assert len(a) == 4
 
 
 def test_stack_will_push_into_stack():
     """Will test that you can .push() into the stack."""
-
     a = Stack((1, 2, 3, 4))
     a.push(5)
     assert a._linked_list.head.data == 5
@@ -30,7 +27,6 @@ def test_stack_will_push_into_stack():
 
 def test_stack_pop_will_pop_top_stack():
     """Will test that the top stack element is popped."""
-
     a = Stack((1, 2, 3, 4))
     a.pop()
     len(a) == 3
@@ -38,21 +34,17 @@ def test_stack_pop_will_pop_top_stack():
 
 def test_stack_pop_returns_proper_item():
     """Will test that popped item is returned."""
-
     a = Stack((1, 2, 3, 4))
     assert a.pop() == 4
 
 
-
 def test_stack_returns_stack_length():
     """Will test that the length of the stack is returned."""
-
     a = Stack()
     assert a._linked_list.size() == 0
 
 
 def test_stack_can_use_len_function():
     """Will test that the len function will return proper length."""
-
     a = Stack()
     assert len(a) == a._linked_list.size()
