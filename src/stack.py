867d32d2d1d393d9ad496fc6bcf74bc02ef15e2e
Philip Werner <philip.r.werner@gmail.com>
2017-10-24 18:09:43 -0700

    wrote tests and stack module, missing pop exception handling

--- src/stack.py
+++ src/stack.py
@@ -1,39 +1,13 @@
 """."""
 
 
-class Node(object):
-    """Node class."""
-
-    def __init__(self, data=None, next_data=None):
-        """Initialize the node structure."""
-        self.data = data
-        self.next_node = next_data
-
-    def get_data(self):
-        """Return current data."""
-        return self.data
-
-    def get_next(self):
-        """Return next data."""
-        return self.next_node
-
-    def set_data(self, newdata):
-        """Set next data."""
-        self.data = newdata
-
-    def set_next(self, new_next):
-        """."""
-        self.next_node = new_next
-
-
 class Stack(object):
-    """."""
+    """Will create a instance of Stack and allow push(), pop() and len() to work properly."""
 
     def __init__(self, iterable=()):
         """Will init a new instance of the Stack class."""
         from linked_list import LinkedList
         self.top_stack = None
-        self._counter = 0
         self._linked_list = LinkedList()
         if isinstance(iterable, (str, tuple, list)):
             for item in iterable:
@@ -46,7 +20,6 @@ class Stack(object):
     def pop(self):
         """Will pop the element that is in the top stack position and subtract it from the counter."""
         return self._linked_list.pop()
-
 
     def __len__(self):
         """Will return the length of the stack using len()."""
--- src/test_stack.py
+++ src/test_stack.py
@@ -1,103 +1,58 @@
 """Tests for stack module."""
 import pytest
+from stack import Stack
 from linked_list import LinkedList
-from linked_list import Node
-
-init_and_insert = [(221, 221), ('word', 'word'), ([1, 2], [1, 2]), (('hi'), ('hi'))]
 
 
-@pytest.mark.parametrize('head, result', init_and_insert)
-def test_noe_and_ll_class_init(head, result):
-    """Test the instance is initialized."""
+def test_init_new_stack():
+    """Will test that the instance is initialized."""
 
-    t = LinkedList(head)
-    n = Node(head)
-    assert n.data == head
-    assert hasattr(n, 'get_next')
-    assert isinstance(t, LinkedList)
-    assert isinstance(t.head, Node)
-    assert t.head.data == result
+    a = Stack()
+    assert hasattr(a, 'top_stack')
+    assert isinstance(a, Stack)
+    assert isinstance(a._linked_list, LinkedList)
 
 
-display_test = [(221, '(221, )'), ('word', '(word, )'), ([1, 2], '([1, 2], )'), ('hi', ('(hi, )'))]
+def test_stack_init_with_iterable():
+    """Will test that input is iterable."""
+
+    a = Stack((1, 2, 3, 4))
+    assert len(a) == 4
 
 
-@pytest.mark.parametrize('head, result', display_test)
-def test_display(head, result):
-    """."""
-    a = LinkedList(head)
-    assert a.display() == result
+def test_stack_will_push_into_stack():
+    """Will test that you can .push() into the stack."""
 
-starter_0 = LinkedList('pos_one')
-
-
-@pytest.mark.parametrize('insert, result', init_and_insert)
-def test_insert(insert, result):
-    """."""
-    starter_0.insert(insert)
-    assert isinstance(starter_0.head, Node)
-    assert starter_0.head.data == result
-    assert starter_0.size() > 1
+    a = Stack((1, 2, 3, 4))
+    a.push(5)
+    assert a._linked_list.head.data == 5
 
 
-starter_1 = LinkedList('pos_one')
-starter_1.insert('pos_two')
-starter_1.insert('pos_three')
-starter_1.insert('pos_four')
-starter_1.insert('pos_five')
+def test_stack_pop_will_pop_top_stack():
+    """Will test that the top stack element is popped."""
+
+    a = Stack((1, 2, 3, 4))
+    a.pop()
+    len(a) == 3
 
 
-@pytest.mark.parametrize('n', range(50))
-def test_size(n):
-    """."""
-    l = LinkedList()
-    for num in range(n):
-        l.insert(num)
-    assert l.size() == n + 1
-    assert hasattr(l, 'size')
-    assert len(l) == n + 1
+def test_stack_pop_returns_proper_item():
+    """Will test that popped item is returned."""
+
+    a = Stack((1, 2, 3, 4))
+    assert a.pop() == 4
 
 
-@pytest.mark.parametrize('one, two, three, search, result', [('1', 4, [33], [33], True), ('word', 33, 3, 88, False), ('po', (22), ('w'), 'pos', True)])
-def test_search(one, two, three, search, result):
-    """."""
-    starter_2 = LinkedList(one)
-    starter_2.insert(two)
-    starter_2.insert(three)
 
-    assert hasattr(starter_2, 'search')
-    try:
-        assert starter_2.search(search)
-    except ValueError:
-        if not result:
-            pass
+def test_stack_returns_stack_length():
+    """Will test that the length of the stack is returned."""
+
+    a = Stack()
+    assert a._linked_list.size() == 0
 
 
-@pytest.mark.parametrize('one, two, result', [(88, '22', '(88, )'), ([22,33], 'hello', '([22, 33], )'), (99, 192, '(99, )')])
-def test_pop(one, two, result):
-    """."""
-    starter_3 = LinkedList(one)
-    starter_3.insert(two)
-    starter_3.pop()
-    assert starter_3.size() == 1
-    assert not starter_3.head.next_node
-    assert starter_3.display() == result
-    try:
-        starter_3.pop()
-    except ValueError:
-        pass
+def test_stack_can_use_len_function():
+    """Will test that the len function will return proper length."""
 
-
-@pytest.mark.parametrize('one, two, de, exist, result', [(77, 'cat', 'cat', True,  '(77, )'), ([3], 1, [33], False, '([3], 1, )')])
-def test_delete(one, two, de, exist, result):
-    """."""
-    starter_3 = LinkedList(one)
-    starter_3.insert(two)
-    assert hasattr(starter_3, 'delete')
-    try:
-        starter_3.delete(de)
-        assert starter_3.display() == result
-    except ValueError:
-        if not exist:
-            pass
-
+    a = Stack()
+    assert len(a) == a._linked_list.size()
