fc451fed6c3a7ad95469cd1fa25d4b08bdd2463c
han8909227 <hbao2016@hotmail.com>
2017-10-31 15:50:43 -0700

    half way done

--- src/binheap.py
+++ src/binheap.py
@@ -1,2 +1,29 @@
 """Binheap data structure."""
 
+
+class Binheap(object):
+    """Binheap class."""
+
+    def __init__(self, iterable=None):
+        """Will init a new instance of the Binheap class."""
+        self.list = [None]
+        self.size = 0
+
+    def push(self, data):
+        """Will push a value into the Binheap class."""
+        self.list.append(data)
+        self.size += 1
+        self._perc_up(self.size)
+
+    def _swap(self, last, parent):
+        """."""
+        self.list[last], self.list[parent] = self.list[parent], self.list[last]
+
+    def _perc_up(self, i):
+        """."""
+        while i // 2 > 0:
+            if self.list[i] < self.list[i // 2]:
+                tmp = self.list[i // 2]
+                self.list[i // 2] = self.list[i]
+                self.list[i] = tmp
+            i = i // 2
