a6b42b71a0238ea8939a41150294274aecf9c0d9
han8909227 <hbao2016@hotmail.com>
2017-10-31 11:55:01 -0700

    refactored tests

--- src/test_deque.py
+++ src/test_deque.py
@@ -6,14 +6,12 @@ from deque import Deque
 @pytest.fixture(scope='function')
 def deque():
     """Making one mt dll instance per test."""
-    from deque import Deque
     return Deque()
 
 
 @pytest.fixture(scope='function')
 def deque_20():
     """Making one dll instance with len of 20 per test."""
-    from deque import Deque
     dq = Deque()
     for num in range(20):
         dq.append(num)
@@ -28,35 +26,22 @@ def test_init_new_dll(deque):
 def test_deque_append_left(deque):
     """Will test the .append_left() function of deque append new value to end."""
     for num in range(20):
-        old_head = deque._dll.head
         deque.append_left(num)
-        assert deque._dll.head.data == num
-        assert deque._dll.head.next == old_head
-        try:
-            assert old_head.previous == deque._dll.head
-        except AttributeError:
-            if num == 0:
-                pass
+        assert num == deque._dll.head.data
 
 
 def test_deque_append(deque):
     """Will test the append() function of deque."""
     for num in range(20):
-        old_tail = deque._dll.tail
         deque.append(num)
-        try:
-            deque._dll.tail.previous == old_tail
-        except AttributeError:
-            if deque._dll.tail == dll.head:
-                pass
+        assert num == deque._dll.tail.data
 
 
 def test_deque_pop(deque):
     """Will test the pop() function of deque will return and remove the tail value."""
     deque.append(1)
-    assert deque.pop() == 1
-    assert deque.size() == 0
-
+    deque.append(2)
+    assert deque.pop() == 2
 
 
 def test_deque_pop_left(deque):
@@ -64,7 +49,6 @@ def test_deque_pop_left(deque):
     deque.append(1)
     deque.append(2)
     assert deque.pop_left() == 1
-    assert deque.size() == 1
 
 
 def test_deque_peek(deque):
@@ -74,6 +58,13 @@ def test_deque_peek(deque):
     assert deque.peek() == 2
 
 
+def test_deque_peek_left(deque):
+    """Testing for the peek_left() function of deque."""
+    deque.append(1)
+    deque.append(2)
+    assert deque.peek_left() == 1
+
+
 def test_deque_peek_after_pop(deque):
     """Testing for the peek() function of deque."""
     deque.append(1)
@@ -81,12 +72,6 @@ def test_deque_peek_after_pop(deque):
     deque.append(3)
     deque.pop()
     assert deque.peek() == 2
-
-def test_deque_peek_left(deque):
-    """Testing for the peek_left() function of deque."""
-    deque.append(1)
-    deque.append(2)
-    assert deque.peek_left() == 1
 
 
 def test_deque_peek_left_after_pop_left(deque):
@@ -99,14 +84,14 @@ def test_deque_peek_left_after_pop_left(deque):
     assert deque.peek_left() == 3
 
 
-def test_deque_size_after_append(deque):
+def test_deque_size(deque):
     """Testing for the peek_left() function of deque."""
     deque.append(1)
     deque.append(2)
     assert deque.size() == 2
 
 
-def test_deque_size_is_none_when_zero(deque):
+def test_deque_size_0(deque):
     """Testing for the peek_left() function of deque."""
     assert deque.size() == 0
 
@@ -137,6 +122,7 @@ def test_len_method_works_on_appended_list(deque):
     deque.append(1)
     deque.append(2)
     assert len(deque) == 2
+
 
 def test_len_method_works_on_append_left_list(deque):
     """Testing that len()returns proper length after appending."""
