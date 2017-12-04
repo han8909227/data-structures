d0206acdf7cf7638c0c4f8e9b99efe570cbb69f0
han8909227 <hbao2016@hotmail.com>
2017-12-04 10:24:25 -0800

    added tests for trie, updated readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='data structure',
     package_dir={'': 'src'},
-    py_modules=['bst', 'graph_1', 'dll', 'stack', 'deque', 'binheap', 'deque', 'que_', 'priorityq', 'hash_'],
+    py_modules=['bst', 'graph_1', 'dll', 'stack', 'deque', 'binheap', 'deque', 'que_', 'priorityq', 'hash_', 'trie'],
     author='Han',
     author_email='hbao2016@hotmail.com',
     description='Data structure graph.',
