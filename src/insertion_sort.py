7c8d8b8235b3ee983b342a6fbc3484a03194e790
han8909227 <hbao2016@hotmail.com>
2017-12-08 15:02:57 -0800

    insertions sort completed

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -1,10 +1,10 @@
-"""My Bubble sort."""
+"""My Insertion sort."""
 import timeit
 from random import randint
 
 
-def bubble_sort(input_list):
-    """Bubble sort."""
+def insertion_sort(input_list):
+    """Insertion sort."""
     if not isinstance(input_list, (list, tuple)):
         raise ValueError('input takes list/tuple only')
 
@@ -16,25 +16,27 @@ def bubble_sort(input_list):
 
     if not all(isinstance(val, (int, float)) for val in input_list):
         raise ValueError('all items in list must be a number')
-    bubble = input_list
-    while True:
-        changed = False
-        for loc in range(len(bubble) - 1):
-            if bubble[loc] > bubble[loc + 1]:
-                bubble[loc + 1], bubble[loc] = bubble[loc], bubble[loc + 1]
-                changed = True
-        if not changed:
-            return bubble
+
+    for index in range(1, len(input_list)):
+        curr = input_list[index]
+        pos = index
+
+        while pos > 0 and input_list[pos - 1] > curr:
+            input_list[pos] = input_list[pos - 1]
+            pos -= 1
+        input_list[pos] = curr
+    return input_list
+
 
 if __name__ == '__main__':  # pragma: no cover
     ordered_list = [num for num in range(10)]
-    t_s = timeit.timeit('bubble_sort(ordered_list) ', setup='from __main__ import bubble_sort, ordered_list')
+    t_s = timeit.timeit('insertion_sort(ordered_list) ', setup='from __main__ import insertion_sort, ordered_list')
     print('BestCase:\n       sorting time: ' + str(t_s) + ' seconds')
 
     reversed_list = ordered_list[::-1]
-    t_s = timeit.timeit('bubble_sort(reversed_list) ', setup='from __main__ import bubble_sort, reversed_list')
+    t_s = timeit.timeit('insertion_sort(reversed_list) ', setup='from __main__ import insertion_sort, reversed_list')
     print('WroseCase:\n      sorting time: ' + str(t_s) + ' seconds')
 
     vals = [randint(0, 1000) for _ in range(10)]
-    t_s = timeit.timeit('bubble_sort(vals) ', setup='from __main__ import bubble_sort, vals')
+    t_s = timeit.timeit('insertion_sort(vals) ', setup='from __main__ import insertion_sort, vals')
     print('RandomCase:\n     sorting time: ' + str(t_s) + ' seconds')
---
+++ src/test_insertion_sort.py
@@ -0,0 +1,47 @@
+"""Test my bubble sort algorithm."""
+from insertion_sort import insertion_sort
+import pytest
+
+
+@pytest.fixture(scope='function')
+def list_ten():
+    """Make a list of 10 vals."""
+    return [x for x in range(10)]
+
+
+def test_sort_nums_in_list(list_ten):
+    """Test bubble sort function."""
+    reverse = list(reversed(list_ten))
+    result = insertion_sort(reverse)
+    assert result == list_ten
+
+
+def test_sort_nums_in_tuple(list_ten):
+    """Test bubble sort function."""
+    reverse = tuple(reversed(list_ten))
+    result = insertion_sort(reverse)
+    assert result == list_ten
+
+
+def test_sort_method_raises_error():
+    """Test if error gets raised for invalid type."""
+    with pytest.raises(ValueError):
+        insertion_sort('12345')
+
+
+def test_sort_method_raises_error_val():
+    """Test if error gets raised for invalid type."""
+    with pytest.raises(ValueError):
+        insertion_sort([1, 2, '3'])
+
+
+def test_sort_method_raise_error_dic():
+    """Test if error gets raised for invalid type."""
+    with pytest.raises(ValueError):
+        insertion_sort({1, 2, 3})
+
+
+def test_sort_method_raise_error_fun():
+    """Test if error gets raised for invalid type."""
+    with pytest.raises(ValueError):
+        insertion_sort([1, 2, 3, 'p'])
