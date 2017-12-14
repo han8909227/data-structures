8c53adb7ee05eda0273bfb167925a4bcfbb2cce3
han8909227 <hbao2016@hotmail.com>
2017-12-14 13:46:49 -0800

    added tests for radix sort

--- src/test_radix_sort.py
+++ src/test_radix_sort.py
@@ -2,6 +2,7 @@
 from radix_sort import radix_sort, _populate_sets, _dequeue_sets
 import pytest
 from random import randint
+from que_ import Queue
 
 
 @pytest.fixture(scope='function')
@@ -22,6 +23,12 @@ def rand_neg():
     return [randint(-1000, 0) for _ in range(100)]
 
 
+@pytest.fixture(scope='session')
+def q_set():
+    """Generate a list of queues."""
+    return [Queue() for _ in range(10)]
+
+
 def test_sort_list_with_neg_values(rand_neg):
     """Test if sorting method sorts negative values."""
     key = sorted(rand_neg)
@@ -34,6 +41,24 @@ def test_sort_nums_in_list_random_case(rand_hundred):
     result = radix_sort(rand_hundred)
     key = sorted(rand_hundred)
     assert result == key
+
+
+def test_populated_set_method(q_set):
+    """Test helper method populate sets."""
+    str_inputs = [str(val) for val in range(10)]
+    indx = 1
+    _populate_sets(str_inputs, indx, q_set)
+    i = 0
+    for q in q_set:
+        val = q.peek()
+        assert int(val) == i
+        i += 1
+
+
+def test_dequeue_set_method(q_set):
+    """Test dequeue set helper method."""
+    result = _dequeue_sets(q_set)
+    assert [int(val) for val in result] == [val for val in range(10)]
 
 
 def test_sort_nums_in_tuple_random_case(rand_hundred):
@@ -81,8 +106,3 @@ def test_sort_method_raise_error_fun():
     with pytest.raises(ValueError):
         radix_sort([1, 2, 3, 'p'])
 
-
-# def test_populated_set_method():
-#     """Test helper method populate sets."""
-#     str_inputs = ['']
-#     _populate_sets(str_inputs, indx, q_set)
