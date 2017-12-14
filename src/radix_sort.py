a6b2df9070a326426e5ba899edab6f33f9e05015
han8909227 <hbao2016@hotmail.com>
2017-12-14 13:00:25 -0800

    radix sort works with neg values

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -5,7 +5,7 @@ from que_ import Queue
 
 
 def radix_sort(input_list):
-    """Quick sort."""
+    """Radix sort."""
     if not isinstance(input_list, (list, tuple)):
         raise ValueError('input takes list/tuple only')
 
@@ -15,20 +15,32 @@ def radix_sort(input_list):
     if not all(isinstance(val, (int, float)) for val in input_list):
         raise ValueError('all items in list must be a number')
 
+    neg = False
+    for num in input_list:
+        if num < 0:
+            neg = True
+            temp = [int(num + 10e10) for num in input_list]
+            input_list = temp
+            break
+
     str_inputs = [str(val) for val in input_list]
     max_val = max(str_inputs, key=len)
     q_set = [Queue() for _ in range(10)]
     indx = 1
-    # import pdb; pdb.set_trace()
-    while indx <= len(max_val):
+
+    for indx in range(1, len(max_val) + 1):
         _populate_sets(str_inputs, indx, q_set)
         str_inputs = _dequeue_sets(q_set)
-        indx += 1
-    return [int(val) for val in str_inputs]
+
+    if neg:
+        return [int(int(val) - 10e10) for val in str_inputs]
+    else:
+        return [int(val) for val in str_inputs]
 
 
 def _populate_sets(str_inputs, indx, q_set):
     """Populate the q set with corresponding indx val of each num."""
+    # import pdb; pdb.set_trace()
     for num in str_inputs:
         try:
             val_at_indx = int(num[-indx])
