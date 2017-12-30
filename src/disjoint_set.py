08d3bbe59fa0a0c7e2254bfe55c6ea9e16f73b62
han8909227 <hbao2016@hotmail.com>
2017-12-30 15:14:05 -0800

    added disjiont set ds

---
+++ src/disjoint_set.py
@@ -0,0 +1,41 @@
+"""
+Disjoint set data structure.
+Disjoint Set data structure (Union–Find), is a data structure that keeps track of a
+set of elements partitioned into a number of disjoint (nonoverlapping) subsets.
+"""
+
+
+class DisjointSet(object):
+    """Disjoint set class."""
+
+    def __init__(self, iterable=None):
+        """Init disjoint set."""
+        self._sets = []
+        for val in iterable:
+            self._sets.append([val])
+
+    def _find_index(self, val):
+        for item in self._sets:
+            if val in item:
+                return self._sets.index(item)
+        return None
+
+    def find(self, val):
+        """Find set a val belongs to."""
+        for item in self._sets:
+            if val in item:
+                return self._sets[self._sets.index(item)]
+        return None
+
+    def union(self, val_1, val_2):
+        """Union two val, putting into one subset."""
+        index_1 = self._find_index(val_1)
+        index_2 = self._find_index(val_2)
+        if index_1 != index_2 and index_1 is not None and index_2 is not None:
+            self._sets[index_2] = self._sets[index_2] + self._sets[index_1]
+            del self._sets[index_1]
+        return self._sets
+
+    def get(self):
+        """Return the current sets."""
+        return self._sets
