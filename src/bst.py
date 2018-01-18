ebc319fdd33c8f2c11263785635387686ab51a72
han8909227 <hbao2016@hotmail.com>
2018-01-18 14:22:09 -0800

    updated bst

--- src/bst.py
+++ src/bst.py
@@ -309,7 +309,7 @@ class BinarySearchTree(object):
             node = node.parent
 
 if __name__ == '__main__':  # pragma: no cover
-    b = BinarySearchTree([20, 10, 5, 15, 3, 7, 13, 17, 30, 25, 23, 27, 35, 37, 23])  # balanced tree depth 3
+    b = BinarySearchTree([20, 10, 5, 15, 3, 7, 13, 17, 30, 25, 23, 27, 35, 37, 33])  # balanced tree depth 3
 
     a = BinarySearchTree()  # all right node tree
     for num in range(0, 15):
